133dab1f5572df3427458a9395f04c51d65ea8f0
Tiit Pikma <thsnr@users.noreply.github.com>
2017-04-22 21:50:27 +0300

    Add physics and movement to Player sprite.

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -1,18 +1,30 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
-  constructor ({ game, x, y, asset }) {
-    
-    super(game, x, y, asset)
+  constructor (game, x, y) {
+    super(game, x, y, 'chars_small', 0)
     
     this.anchor.setTo(0.5)
-    this.checkWorldBounds = true
-    this.events.onOutOfBounds.add(::this.onOutOfBounds, this)
+
+    this.game.physics.arcade.enable(this)
+    this.body.drag.x = this.body.drag.y = 500
+
+    this.cursors = this.game.input.keyboard.createCursorKeys()
   }
 
-  update () {}
+  update () {
+    const acc_v = 300
+    if (this.cursors.up.isDown) {
+      this.body.velocity.y = -acc_v
+    } else if (this.cursors.down.isDown) {
+      this.body.velocity.y = acc_v
+    }
 
-  onOutOfBounds () {
-
+    const acc_h = 500
+    if (this.cursors.left.isDown) {
+      this.body.velocity.x = -acc_h
+    } else if (this.cursors.right.isDown) {
+      this.body.velocity.x = acc_h
+    }
   }
 }

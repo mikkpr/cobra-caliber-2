4253df13c14a60ab78c9dcfe659fd32abacfa17a
Mikk Pristavka <mikk@voog.com>
2017-04-22 22:13:36 +0300

    Minor code style tweaks

--- src/states/Fight.js
+++ src/states/Fight.js
@@ -6,12 +6,6 @@ import Player from '../sprites/Player'
 import Curve from '../plugins/Curve'
 
 export default class extends Phaser.State {
-  init () {}
-
-  preload () {
-
-  }
-
   create () {
     this.game.world.enableBody = true
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
@@ -19,7 +13,7 @@ export default class extends Phaser.State {
 
     this.map = this.game.add.tilemap('moon_fight')
     this.map.addTilesetImage('lofi_environment_4x', 'tiles')
-    
+
     // Add both the background and ground layers. We won't be doing anything
     // with the GroundLayer though
     this.backgroundLayer = this.map.createLayer('backgroundlayer')
@@ -29,21 +23,21 @@ export default class extends Phaser.State {
     this.map.setCollisionBetween(1, 100, true, 'groundlayer')
 
     // Add the sprite to the game and enable arcade physics on it
-    var player_spawn_x = this.game.world.centerX / 2;
-    var boss_spawn_x = this.game.world.centerX + this.game.world.centerX / 2;
+    var playerSpawnX = this.game.world.centerX / 2
+    var bossSpawnX = this.game.world.centerX + this.game.world.centerX / 2
 
-    this.player = new Player(this.game, player_spawn_x, this.game.world.centerY)
+    this.player = new Player(this.game, playerSpawnX, this.game.world.centerY)
     this.world.add(this.player)
 
-    this.boss = new Boss(this.game, boss_spawn_x, this.game.world.centerY)
+    this.boss = new Boss(this.game, bossSpawnX, this.game.world.centerY)
     this.world.add(this.boss)
 
     // Change the world size to match the size of this layer
     this.groundLayer.resizeWorld()
 
     // Add gravity to the sprites.
-    addGravity(this.player);
-    addGravity(this.boss);
+    addGravity(this.player)
+    addGravity(this.boss)
 
     // Make the camera follow the player.
     this.game.camera.follow(this.player)
@@ -54,10 +48,9 @@ export default class extends Phaser.State {
     this.game.physics.arcade.collide(this.player, this.groundLayer)
     this.game.physics.arcade.collide(this.boss, this.groundLayer)
   }
-
 }
 
-function addGravity(item) {
+function addGravity (item) {
   item.body.bounce.y = 0.3
   item.body.gravity.y = 1000
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -7,14 +7,14 @@ export default class extends Phaser.State {
   preload () {
     this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
     this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-    
+
     centerGameObjects([this.loaderBg, this.loaderBar])
 
     this.load.setPreloadSprite(this.loaderBar)
 
     this.load.spritesheet('chars_small', 'assets/images/lofi_char_4x.png', 32, 32)
     this.load.spritesheet('chars_large', 'assets/images/lofi_char_4x.png', 64, 64)
-    
+
     this.load.tilemap('moon_fight', 'assets/tilemaps/moon_fight.json', null, Phaser.Tilemap.TILED_JSON)
     this.load.tilemap('earth_travel', 'assets/tilemaps/earth_travel.json', null, Phaser.Tilemap.TILED_JSON)
 

64820d532b0da4a0d9377440a5b44975da674e1d
Juhan Trink <juhan@trink.ee>
2017-04-22 22:25:56 +0300

    add gamepad support

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -10,6 +10,10 @@ export default class extends Phaser.Sprite {
     this.body.drag.x = this.body.drag.y = 500
 
     this.cursors = this.game.input.keyboard.createCursorKeys()
+	
+	// Gamepad support
+	game.input.gamepad.start();
+	
   }
 
   update () {
@@ -19,12 +23,26 @@ export default class extends Phaser.Sprite {
     } else if (this.cursors.down.isDown) {
       this.body.velocity.y = acc_v
     }
-
+	
+	if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1) {
+		this.body.velocity.y = -acc_v;
+    }
+    else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1) {
+		this.body.velocity.y = acc_v;
+    }
+	
     const acc_h = 500
     if (this.cursors.left.isDown) {
       this.body.velocity.x = -acc_h
     } else if (this.cursors.right.isDown) {
       this.body.velocity.x = acc_h
+    }
+	
+	if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1) {
+        this.body.velocity.x = -acc_h;
+    }
+    else if (pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1) {
+        this.body.velocity.x = acc_h;
     }
   }
 }

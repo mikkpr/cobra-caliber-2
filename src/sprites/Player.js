1eb25fbb785bd6f1aacdac17fcd721ac452a62b0
Aare Undo <aare@carto.com>
2017-04-23 12:55:38 +0300

    re-introduced wasd movement

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -10,7 +10,14 @@ export default class extends Phaser.Sprite {
     this.body.drag.x = this.body.drag.y = 500
 
     this.cursors = this.game.input.keyboard.createCursorKeys()
-
+    
+    this.wasd = {
+      up: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
+      down: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
+      left: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
+      right: this.game.input.keyboard.addKey(Phaser.Keyboard.D),
+    };
+    
     this.game.input.gamepad.start()
 
     // Add a bitchin trail because we are going supersonic
@@ -58,16 +65,16 @@ export default class extends Phaser.Sprite {
     const pad1 = this.game.input.gamepad.pad1
 
     const accV = 300
-    if (this.cursors.up.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1) {
+    if (this.cursors.up.isDown || this.wasd.up.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1) {
       this.body.velocity.y = -accV
-    } else if (this.cursors.down.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1) {
+    } else if (this.cursors.down.isDown || this.wasd.down.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1) {
       this.body.velocity.y = accV
     }
 
     const accH = 500
-    if (this.cursors.left.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1) {
+    if (this.cursors.left.isDown || this.wasd.left.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1) {
       this.body.velocity.x = -accH
-    } else if (this.cursors.right.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1) {
+    } else if (this.cursors.right.isDown || this.wasd.right.isDown || pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1) {
       this.body.velocity.x = accH
     }
   }

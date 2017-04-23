94367c3d30724ec339b99148fa6c34c405cc2373
Tiit Pikma <thsnr@users.noreply.github.com>
2017-04-23 23:28:57 +0300

    Rework player movement in Travel.

--- src/states/Travel.js
+++ src/states/Travel.js
@@ -31,7 +31,16 @@ export default class extends Phaser.State {
     // Add the sprite to the game and enable arcade physics on it
     this.player = new Player(this.game, 100, 256, { canTurn: false, isFalling: true })
     this.world.add(this.player)
+
+    // Player physics in this state.
+    this.maxVelocity = 800 // Default max velocity, can be overridden by dashing.
+    this.player.body.maxVelocity.x = this.maxVelocityX
+    this.player.body.maxVelocity.y = this.maxVelocity
     this.player.body.gravity.x = 1800
+
+    this.player.body.collideWorldBounds = true
+    this.player.body.onWorldBounds = new Phaser.Signal()
+    this.player.body.onWorldBounds.add(this.hitWorldBounds, this)
 
     // Add turrets.
     new Array( // Use new Array instead of [] so webpack does not get confused.
@@ -57,10 +66,6 @@ export default class extends Phaser.State {
     this.game.scale.refresh()
 
     this.game.time.advancedTiming = true
-
-    this.player.body.collideWorldBounds = true
-    this.player.body.onWorldBounds = new Phaser.Signal()
-    this.player.body.onWorldBounds.add(this.hitWorldBounds, this)
   }
 
   render () {
@@ -71,6 +76,26 @@ export default class extends Phaser.State {
   update () {
     this.game.camera.x = this.player.x - this.game.width / 2
     this.game.physics.arcade.collide(this.player, this.groundLayer, this.player.resetWithAnimation, null, this.player)
+
+    const accY = 800
+    if (this.player.isMovingUp()) {
+      this.player.body.velocity.y -= accY
+    } else if (this.player.isMovingDown()) {
+      this.player.body.velocity.y += accY
+    } else {
+      this.player.body.velocity.y /= 2
+    }
+
+    const accX = 360
+    if (this.player.isMovingRight()) {
+      this.player.body.maxVelocity.x = this.maxVelocity + accX
+      this.player.body.velocity.x += accX
+    } else if (this.player.isMovingLeft()) {
+      this.player.body.maxVelocity.x = this.maxVelocity - accX
+      this.player.body.velocity.x -= accX / 6 // Braking takes time.
+    } else {
+      this.player.body.maxVelocity.x = this.maxVelocity
+    }
   }
 
   shutdown () {

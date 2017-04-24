d250f5d942a2b1dd78878a0a162c008ccfb36ada
Mikk Pristavka <mikk@voog.com>
2017-04-24 13:30:48 +0300

    Tweak sounds

--- src/sounds.js
+++ src/sounds.js
@@ -1,10 +1,10 @@
 export default (game) => {
   const textSound = game.add.audio('step', 0.25)
   const explodeSound = game.add.audio('explode', 0.25)
-  const shootSound = game.add.audio('shoot', 0.25)
+  const shootSound = game.add.audio('shoot', 0.10)
   const clickSound = game.add.audio('click', 0.25)
   const impactSound = game.add.audio('impact', 0.45)
-  const bulletMiss = game.add.audio('bulletmiss', 0.05)
+  const bulletMiss = game.add.audio('bulletmiss', 0.25)
 
   textSound.allowMultiple = true
   explodeSound.allowMultiple = true
